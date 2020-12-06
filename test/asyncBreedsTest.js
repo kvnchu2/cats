751f2d5f7a755a78ceed8ca2016fe1487b0089fb
Kevin Chu <kvnchu2@gmail.com>
2020-12-06 23:46:11 +0000

    added in extra code for stretch assignment

---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+describe('#breedDetailsFromFile', () => {
+  it('provides, via callback, breed details for the Bombay breed', (done) => {
+    breedDetailsFromFile('Balinese', (bombay) => {
+      const expectedDesc = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+      assert.equal(expectedDesc, bombay);
+      //console.log(expected === actual)
+      
+      done(); 
+    });      
+  });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      assert.equal(undefined, desc);
+      done(); 
+    });      
+  });
+});
+
